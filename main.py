1425ecf4ceb79d8f508b136019bff3ed87d05fd9
Enrico-Medina <161720504+Enrico-Medina@users.noreply.github.com>
2024-04-23 10:58:52 -0300

    Update main.py

--- main.py
+++ main.py
@@ -24,6 +24,27 @@ atividade_fisica = st.number_input("Escolha entre as opções a sua frequência 
 st.write("1 - sedentário, 2 - levemente ativo, 3 - moderadamente ativo, 4 - muito ativo, 5 - super ativo")
 st.write("---")
 
+# Criando a função de TMB
+def calcular_nutrientes(sexo, peso, altura, idade, atividade_fisica):
+    # Equação de Mifflin-St Jeor
+    tmb_base = (10 * peso) + (6.25 * altura) - (5 * idade) + (5 if sexo == 1 else -161)
+    fatores_atividade = [1.2, 1.375, 1.55, 1.725, 1.9]
+    tmb_ajustada = tmb_base * fatores_atividade[atividade_fisica - 1]
+    if tmb_ajustada <= 1700:
+      tmb_ajustada += 300
+
+    # Dict com os macro-nutrientes ajustados baseado no TMB
+    nutrientes = {
+        'tmb': round(tmb_ajustada,2),
+        'prot': round(tmb_ajustada * 0.25/4,2),
+        'carbs': round(tmb_ajustada * 0.55/4,2),
+        'fats': round(tmb_ajustada * 0.22/4,2),
+        'calcium': 1000 if idade < 50 else 1200,
+        'magnesium': 400,
+        'zinc': 12
+    }
+    return nutrientes
+
 if st.button('Calcular Nutrientes'):
     resultado = calcular_nutrientes(sexo, peso, altura, idade, atividade_fisica)
     st.write("Resultado:")
